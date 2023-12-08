63c561671b27f94301aa9e05d500e9c124167028
Даниил Ключников <d.klyuchnikov@gbubr.local>
2023-12-08 16:17:10 +0300

    BpmnAnalyzPage изменен

--- src/components/BpmnAnalyzPage.js
+++ src/components/BpmnAnalyzPage.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BpmnDiagram from './BpmnDiagram';
+import XsdReader from './XsdReader';
 
-const BpmnAnalyzPage = ({ xmlData }) => {
+const BpmnAnalyzPage = () => {
+  const [xmlData, setXmlData] = useState(null);
+  const [fileName, setFileName] = useState('');
+
+  const handleXmlChange = (xml, name) => {
+    setXmlData(xml);
+    setFileName(name);
+  };
+
   return (
     <div>
-      <h2>BPMN Diagram</h2>
-      <BpmnDiagram xml={xmlData} />
+      <XsdReader onXmlChange={handleXmlChange} />
+
+      {xmlData && (
+        <div>
+          <h3>Анализ BPMN файла: {fileName}</h3>
+          <BpmnDiagram xml={xmlData} />
+        </div>
+      )}
     </div>
   );
 };
